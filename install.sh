48707c9e5b37f59f0c83814494afe4f72683bfce
IshunChin <topjun0424@gmail.com>
2019-07-10 00:16:12 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
-#/bin/bash
+#!/bin/bash
 
-apt update
-apt upgrade -y
-apt install -y curl git nodejs npm python3 pipenv python3-pip fzf silversearcher-ag vim
-pip3 install awscli
+apt install -y curl git nodejs npm python3 pipenv python3-pip fzf silversearcher-ag vim tmux
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.vim/plugged
+cd ~
+mkdir -p ~/.vim/plugged
 git clone https://github.com/IshunChin/dotfile.git
 git clone https://github.com/crusoexia/vim-monokai.git ~/.vim/plugged/vim-monokai
 
 ln `pwd`/dotfile/vimrc ~/.vimrc
 ln `pwd`/dotfile/tmux.conf ~/.tmux.conf
 
-
-vim +PlugInstall +qa
+vim +PlugInstall +qall
+vim +"CocInstall coc-python" +qall
